84f0eff4d25e791dc1d7e8a82a0413a51e0c4ac0
Dominik Stolarz <73101500+dommnk@users.noreply.github.com>
2021-08-19 18:28:35 +0200

    Add files via upload

---
+++ StackOverflowPost/Post.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace StackOverflowPost
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime PostDate { get; private set; }
+        private int _score;
+
+        public Post(string title)
+        {
+            this.Title = title;
+            _score = 0;
+            PostDate = DateTime.Now;
+        }
+
+        public Post(string title, string description)
+            :this(title)
+        {
+            this.Description = description;
+        }
+
+        public void Vote(string sign)
+        {
+            if (sign == "+")
+                _score += 1;
+            else if (sign == "-")
+                _score -= 1;
+
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Title: " + Title);
+            Console.WriteLine("Description: " + Description);
+            Console.WriteLine("Publication Date: " + PostDate);
+            Console.WriteLine("Post score is now: " + _score);
+        }
+    }
+}
---
+++ StackOverflowPost/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace StackOverflowPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string signInput;
+            string titleInput;
+            string descriptionInput;
+
+            Console.WriteLine("Enter title:");
+            titleInput = Console.ReadLine();
+            Console.WriteLine("Enter description:");
+            descriptionInput = Console.ReadLine();
+
+            var post= new Post(titleInput, descriptionInput);
+            // var post = new Post("I am new to programming", "Please help me");
+            
+            post.Display();
+
+            Console.WriteLine("Type(+/-) to upvote or downvote post. Q to quit.");
+
+            while(true)
+            {
+                signInput = Console.ReadLine();
+
+                if (signInput != "Q" && signInput != "q")
+                    post.Vote(signInput);
+                else
+                    break;
+            }
+            
+            post.Display();
+        }
+    }
+}
