3e05bb080f13ee427eb8de7d954886141f925419
Dominik Stolarz <73101500+dommnk@users.noreply.github.com>
2021-08-23 20:24:02 +0200

    Add files via upload

---
+++ Stack/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object ElementProvided;
+            var stack = new Stack();
+
+            Console.WriteLine(@"Type in an object to push on stack
+(Q to quit, DSP to display elements in stack,
+POP to pop an element, CLR to clear list.)");
+
+            ElementProvided = Console.ReadLine();
+            stack.ExecuteStack(ElementProvided);
+            
+        }
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+
+namespace Stack
+{
+
+    public class Stack
+    {
+        private ArrayList list = new();
+        private int ElementCount = 0;
+        public  void ExecuteStack(object ElementProvided)
+        {
+            while ((string)ElementProvided != "Q")
+            {
+                switch (ElementProvided)
+                {
+                    case "DSP":
+                        DisplayAllElements();
+                        break;
+                    case "POP":
+                        Pop();
+                        break;
+                    case "CLR":
+                        Clear();
+                        break;
+                    default:
+                        Push(ElementProvided);
+                        break;
+                }
+
+                ElementProvided = Console.ReadLine();
+            }
+
+            Console.WriteLine("Closing an application.");
+        }
+        public void Push(object element)
+        {
+            if (!String.IsNullOrEmpty((string)element)) 
+            {
+                list.Add(element);
+                Console.WriteLine("{0} has been added to the list.", element);
+                ElementCount++;
+            }
+            else
+            {
+                Console.WriteLine("Cannot add an empty object to stack.");
+            }
+        }
+        
+        public void Pop()
+        {
+            object toPop;
+
+            if(ElementCount > 0)
+            {
+                toPop = list[ElementCount - 1];
+                list.Remove(toPop);
+                Console.WriteLine("{0} has been popped from the list.", toPop);
+                ElementCount--;
+            }
+            else
+            {
+                Console.WriteLine("There are no elements in stack.");
+            }
+        }
+
+        public void Clear()
+        {
+            list.Clear();
+            Console.WriteLine("Stack has been cleared.");
+            ElementCount = 0;
+        }
+        public void DisplayAllElements()
+        {
+            if(ElementCount != 0)
+                Console.WriteLine("All elements in stack are listed below: ");
+            else
+                Console.WriteLine("There is nothing in the stack.");
+
+            int index = 1;
+
+            foreach (object element in list)
+            {
+                Console.WriteLine("{0}. {1}", index, element);
+                index++;
+            }
+        }
+
+    }
+}
